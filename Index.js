d15a8953f3cfdabe0cb3b157cfd76ae94599bf4b
darkplayerxd <jsebastiancastano417@gmail.com>
2022-02-24 07:44:12 -0500

    Taller

---
+++ Index.js
@@ -0,0 +1,50 @@
+var Nombre="CosteñoMoment";
+var Apellido="Castaño";
+console.log("Hola mundo");
+console.log(Nombre);
+console.log(Apellido);
+
+
+//Comentario en linea
+
+//Variables
+/*Son espacios de memoria 
+que almacacenan informacion
+existen varios tipos de variables en js,
+pero generalmente se ultilizan los siguientes : 
+String : Almacenan cadenas de texto, se debe 
+declarar o asignar el valor entre comillas.
+Int : Almacenan numeros enteros.
+Double : Almacenan numeros reales.
+Boolean : Almacenan valores de verdad (True o Faslse).
+
+/*Declarar una varible
+es crearla, decirle al lenguaje 
+como se llama la variable e indicarle que ya existe.
+
+Para declarar una variable se puede utilizar ya sea
+la palabra reservada "var o let" 
+*/
+
+/*Inicializar variables
+
+
+es necesario asignar un valor cuando se crea esta variable*/ 
+//Desde otra variable
+
+var edad = 16;
+var edadestudiante = edad;
+
+//Desde el teclado
+
+var gradoestudiante = prompt("Ingrese un grado");
+console.log ("El grado es : " +gradoestudiante);
+
+/*Las variables
+se crean con la palabra reservada cons
+se escribe todo en mayusculas para
+diferenciarlo de las variables*/
+
+const PI = 3.1416;
+
+//
