58f32f435a8193e8ea32bba43903f11d6e267014
darkplayerxd <jsebastiancastano417@gmail.com>
2022-02-24 08:10:44 -0500

    Taller finalizado

---
+++ Taller.js
@@ -0,0 +1,12 @@
+//Archivo de práctica y pruebas de JavaScript
+
+/*Taller 1
+Fecha 21/02/2022
+Realizado por:Juan Sebastian Castaño*/
+
+var Nombre="juan sebastian castaño"; //Variable tipo string
+var Grado ="10B";                    //Variable tipo string
+var Edad = prompt("Ingrese su edad");//Variable tipo int
+console.log ("“El estudiante " +Nombre+ " del grado : " +Grado+" , tiene " +Edad+ " años")
+
+
